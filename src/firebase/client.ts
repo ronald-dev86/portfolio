12fb993b0451fbde832aef91a974b57e86c75f06
Ronald Penaloza <ronald.dev86@gmail.com>
2025-01-05 14:58:15 -0400

    first commit

---
+++ src/firebase/client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGW2QX1IeYSQLzWQtTe35ipTcmW9hI2z0",
+  authDomain: "metro-cubico-8daad.firebaseapp.com",
+  projectId: "metro-cubico-8daad",
+  storageBucket: "metro-cubico-8daad.appspot.com",
+  messagingSenderId: "902277520427",
+  appId: "1:902277520427:web:809d5570d800faeddb1e6e"
+}
+
+export const app = initializeApp(firebaseConfig);
