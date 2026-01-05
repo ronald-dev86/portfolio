a2209b02c83f85a6bb5e42a2f3f29dff1f918ec6
Ronald Penaloza <ronald.dev86@gmail.com>
2026-01-05 12:34:17 -0400

    change clean code

---
+++ src/data/config.ts
@@ -0,0 +1,33 @@
+export const socialLinks = {
+  linkedin: {
+    url: "https://www.linkedin.com/in/ronald-pe%C3%B1aloza/",
+    icon: "linkedin",
+    label: "LinkedIn"
+  },
+  github: {
+    url: "https://github.com/ronald-dev86",
+    icon: "github",
+    label: "GitHub"
+  },
+  instagram: {
+    url: "https://www.instagram.com/rpenaloza.dev",
+    icon: "instagram",
+    label: "Instagram"
+  },
+  whatsapp: {
+    url: "https://wa.me/584129653465",
+    icon: "whatsapp",
+    label: "WhatsApp"
+  },
+  telegram: {
+    url: "https://t.me/ronalddev86",
+    icon: "paper-plane-outline",
+    label: "Telegram"
+  }
+};
+
+export const cvLink = {
+  url: "/file/fullstack_con foto.pdf",
+  icon: "reader",
+  label: "Mi CV"
+};
